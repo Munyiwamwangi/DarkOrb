9c17e7e15a88d7dd7ef0a9b06faa5d313c3edc7a
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-29 16:12:17 +0300

    add display files for testing

--- darkorb/views.py
+++ darkorb/views.py
@@ -3,15 +3,16 @@ import requests
 
 # Create your views here.
 def home(requests):
-	response = requests.get(https://api.themoviedb.org/3/movie/550?api_key=0e53893601f4571b5f08dafad21f6272)
-	moviedata = respnse.json()
-	return render(reqiest, 'home.html', {
-		'id': moviedata('movie_id'),
-		'overview': geodata ['movie_overview']
-		'poster': geodata['movie_poster'],
-        'title': geodata['movie_title'],
-        'vote_count': geodata['latitude'],
-        'poster_path': geodata['movie_poster_path']
-        'vote_count': geodata['vote_count']
+	url = 'https://api.themoviedb.org/3/movie/550?api_key=0e53893601f4571b5f08dafad21f6272'
+	response = requests.get('https://api.themoviedb.org/3/movie/550?api_key=0e53893601f4571b5f08dafad21f6272')
+	moviedata = response.json()
+	return render(request, 'home.html', {
+		'id': moviedata['id'],
+		'title': moviedata['title'],
+		# 'genres': moviedata['genres['name']'],
+		'overview': moviedata ['overview']
+		'release_date': moviedata['release_date'],
+        'poster_path': moviedata['poster_path'],
+        'vote_count': moviedata['vote_count'],
 
 		})
