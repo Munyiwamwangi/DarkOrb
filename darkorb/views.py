a843b232991f173a1a5ae17ba18c7b8b2341be51
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-29 16:11:52 +0300

    add def for fetching API elements

--- darkorb/views.py
+++ darkorb/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def home(requests):
+	response = requests.get(https://api.themoviedb.org/3/movie/550?api_key=0e53893601f4571b5f08dafad21f6272)
+	moviedata = respnse.json()
+	return render(reqiest, 'home.html', {
+		'id': moviedata('movie_id'),
+		'overview': geodata ['movie_overview']
+		'poster': geodata['movie_poster'],
+        'title': geodata['movie_title'],
+        'vote_count': geodata['latitude'],
+        'poster_path': geodata['movie_poster_path']
+        'vote_count': geodata['vote_count']
+
+		})
